dd394b2df287776924118f13b855e63660be6b09
Daohuy2409 <100114549+Daohuy2409@users.noreply.github.com>
2024-09-17 21:24:39 +0700

    Update WebApplication.java

--- src/main/java/com/example/web/WebApplication.java
+++ src/main/java/com/example/web/WebApplication.java
@@ -1,17 +1,13 @@
 package com.example.web;
 
-import com.example.web.service.EmailService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.RequestMapping;
 
-import java.io.IOException;
 
 
 @SpringBootApplication
 public class WebApplication {
 
-	private EmailService emailService;
 	public static void main(String[] args) {
 		SpringApplication.run(WebApplication.class, args);
 	}
